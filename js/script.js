f3d6c01d8342e742611b9af50c468013c5a3dc00
scusate <nevadialonso@gmail.com>
2022-10-12 19:20:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var index = document.getElementsByClassName('image');
+var length = index.length;
+var slide = 0;
+
+for (var i = 1; i < index.length; i++) {
+    index[i].style.display = 'none';
+}
+document.getElementById('right').onclick = function Right () {
+    if(slide == length-1){
+        index[0].style.display = "block";
+        index[slide].style.display = "none";
+        slide = 0;
+    } else {
+        index[slide+1].style.display = "block";
+        index[slide].style.display = "none";
+        slide++;
+    }
+}
+
+document.getElementById('left').onclick = function Left () {
+    if(slide <= 0) {
+        index[slide].style.display = "none";
+        slide = length;
+        slide--;
+        index[slide].style.display = "block";
+    } else {
+        index[slide].style.display = "none";
+        slide--;
+        index[slide].style.display = "block";
+    }
+}
